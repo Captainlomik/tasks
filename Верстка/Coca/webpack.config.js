e7b72182beb1522baf719e345a9e1e4141cdc22e
captainlomik <nastiy248@mail.ru>
2023-07-01 21:12:34 +0300

    add

---
+++ Верстка/Coca/webpack.config.js
@@ -0,0 +1,48 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const FileManagerPlugin = require('filemanager-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    entry: path.join(__dirname, 'src', 'index.js'),
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'index.[contenthash].js',
+        assetModuleFilename: path.join('assets', '[name].[contenthash][ext]'),
+    },
+    devServer: {
+        watchFiles: path.join(__dirname, 'src'),
+        port: 9000,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(scss|css)$/,
+                use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
+            },
+            {
+                test: /\.(png|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.svg$/,
+                type: 'asset/resource',
+                generator: {
+                    filename: path.join('icons', '[name].[contenthash][ext]'),
+                },
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'src', 'index.html'),
+            filename: 'index.html',
+        }),
+        new FileManagerPlugin({
+            events: {
+                onStart: {
+                    delete: ['dist'],
+                },
+            },
+        }),
+    ],
+};
